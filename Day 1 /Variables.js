5e31cc0832fb035bd6eb1c0f03c2ef726c413488
Lokesh Medharimetla <73935267+lokesh-7977@users.noreply.github.com>
2024-07-17 09:59:50 +0000

    "Challenges - Variables & Typeof Optr"

---
+++ Day 1 /Variables.js
@@ -0,0 +1,31 @@
+
+
+var a  = 10;
+console.log(a)
+let b = "Lokesh"
+console.log(b)
+console.log(typeof(b))
+
+const c = true
+console.log(true)
+console.log(typeof(c))
+
+const n = 123
+console.log(typeof(n))
+
+const arr = [1,2,3,4]
+console.log(typeof(arr))
+
+const obj = {
+    name : "Loki",
+    uru : "Dharamaram"
+}
+console.log(typeof(obj))
+
+let  k =9;
+console.log(k)
+
+const m = 90;
+console.log(m)
+
+
