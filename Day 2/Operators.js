b7bbcd9cc51ccccbc3574a3deaf8e931f0f8f32c
Lokesh Medharimetla <73935267+lokesh-7977@users.noreply.github.com>
2024-07-17 10:24:47 +0000

    "Challenges Day 2 - Operators "

---
+++ Day 2/Operators.js
@@ -0,0 +1,52 @@
+// Add Two Numbers 
+let a = 5
+let b = 6
+let add = a + b
+console.log(add) // 11
+
+// Subtract two numbers
+let sub = a - b 
+console.log(sub) // -1
+
+// Multiply two numbers
+let mul = a * b // 30
+console.log(mul)
+
+// Divide two numbers
+let div = a / b 
+console.log(div) // 0.8333333333333334
+
+// Modulus of two numbers
+let mod = a % b
+console.log(mod) // 5
+
+
+//Assignment Optrs
+ a += 5
+ console.log(a) // 10
+ b-= 5
+console.log(b) // 1
+
+
+// Comparison Operators
+console.log(a<b)
+console.log(a>b)
+console.log(a<=b)
+console.log(a>=b)
+
+// Logical Operators
+console.log(a<b && a>b)
+
+console.log(a<b || a>b)
+
+console.log(!(a<b))
+
+
+console.log(a==b)
+
+console.log(a!=b)
+
+// Ternary Operator
+let n = 8;
+let m = 0;
+console.log(m>1 && n>1 ? "positive" : "negative")
